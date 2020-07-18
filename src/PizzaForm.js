45dbd85b003cd4ee40afa4adf198199bb71fc4a1
EmilyLow <emlow@me.com>
2020-07-17 19:13:27 -0500

    name and dropdown added

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect } from "react";
 
 const PizzaForm = props => {
   
-  
+    const defaultState = {
+        name: ""
+    }
+
+    const [formState, setFormState] = useState(defaultState);
+
+
+    const handleChange = e => {
+
+        setFormState({
+            ...formState, [e.target.name]: e.target.value
+        });
+
+        //Put inputChange(e) to test here
+    }
+
+
+    const submitData = e => {
+
+    }
   
     return (
-      <p>Pizza Form</p>
+      
+      <div className="form">
+          <form onSubmit={submitData}>
+            <label>
+                Name:
+                <input
+                type = "text"
+                name = "name"
+                value = {formState.name}
+                onChange = {handleChange}
+                className = "name-field"
+                />
+            </label>
+            <label>
+                Pizza Size:
+                <select id="size" name="size" onChange={handleChange}>    
+                    <option value="Small">Small</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Large">Large</option>
+                </select>
+
+            </label>
+            
+
+
+
+
+          </form>
+
+      </div>
   );
 };
 
